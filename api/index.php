80133e23eccd5492f8e6f61b6360dca5bb82381c
freddan88 <fredrik@leemann.se>
2022-01-02 10:55:48 +0100

    Updated request from uri and moved request_method to top

--- api/index.php
+++ api/index.php
@@ -2,8 +2,9 @@
 
 declare(strict_types=1);
 
-require_once('./app/Security.php');
 require_once('./app/Response.php');
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') Response::error($httpStatuses[405]);
 
 $configuration = parse_ini_file("config.ini", true);
 
@@ -13,21 +14,22 @@ $httpStatuses = $configuration['http_codes'];
 
 $allowedKey = $configuration['security']['api_key'];
 
+require_once('./app/Security.php');
+
 $currentOrigin = Security::getRemoteOrigin();
 
 if (!Security::validateRemoteOrigin($currentOrigin, $allowedOrigins)) {
     Response::error($httpStatuses[403], $currentOrigin);
 }
 
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') Response::error($httpStatuses[405]);
-
 if (empty($_POST['apikey'])) Response::error($httpStatuses[400]);
 
 if ($_POST['apikey'] !== $allowedKey) Response::error($httpStatuses[403]);
 
 $req = $_SERVER['REQUEST_URI'];
-$req = trim($req, '/');
 $req = parse_url($req, PHP_URL_PATH);
+$req = explode("/", $req);
+$req = end($req);
 
 require_once('./app/Email.php');
 
@@ -35,5 +37,5 @@ if ($req === 'email') {
     $email = new Email($_POST);
     if ($email->validate()) $email->send();
 } else {
-    Response::error($httpStatuses[400]);
+    Response::error($httpStatuses[503]);
 }
